d3e721945a945f413c40fb877643ee392a2d6ace
rj-002 <rjchamp002@gmail.com>
2025-04-16 22:33:09 +0530

    Refactor: Load SECRET_KEY from .env

--- TodoList/TodoList/settings.py
+++ TodoList/TodoList/settings.py
@@ -11,8 +11,10 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
-
+from dotenv import load_dotenv
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
+load_dotenv()
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
@@ -20,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-hqq69%yx63nz6k)*dc8t!^zym#sfs^03hg4_ytao0lgixv*h55'
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -125,3 +127,4 @@ LOGOUT_REDIRECT_URL = 'login'  # optional
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
